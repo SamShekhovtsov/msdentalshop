cb06bef892648922cc7ae4e0db93b2166afb9ba0
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-04-06 21:27:00 +0800

    email configuration changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,23 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 578,
+    domain: Rails.application.secrets.email_provider_name,
+    password: Rails.application.secrets.email_provider_password
+  }
+
+  config.action_mailer.default_url_options = {
+    :host => 'localhost:3000'
+  }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+
+  # Send email in development mode?
+  config.action_mailer.perform_deliveries = true
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
