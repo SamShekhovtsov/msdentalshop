846cac3497ef9c53d5b2cab91a3bed6da8b0ab78
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-04-09 21:02:28 +0800

    adding application mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ApplicationMailer
+    defult from: "semshehovtsov@gmail.com"
+
+    def contact_email(contact)
+        @contact = contact
+        mail(to: Rails.application.secrets.owner_email, from: @contact.email, :subject => "Website Contact")
+    end
+end
