e7e242f8f27ec039329927d8e50d8e7bcbe5a58a
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-04-11 04:08:18 +0800

    configuration fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
-    default from: "semshehovtsov@gmail.com"
+    default from: Rails.application.credentials.gmailsmtp[:username]
 
     def contact_email(contact)
         @contact = contact
-        mail(to: Rails.application.secrets.owner_email, from: @contact.email, :subject => "Website Contact")
+        mail(to: Rails.application.credentials.gmailsmtp[:clinicinbox], from: @contact.email, :subject => "MS Dental clinic service appointment request") # Rails.application.secrets.owner_email
     end
 end
