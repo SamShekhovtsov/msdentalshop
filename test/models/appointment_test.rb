b58b9e80f93d8e104dafef063b296a44fe909865
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-04-17 22:54:44 +0800

    tests for the appointment model

--- test/models/appointment_test.rb
+++ test/models/appointment_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class AppointmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not make appointments without appointment date" do
+    appointment = Appointment.new
+    assert_not appointment.save, "Created appointment without appointment date"
+  end
 end
