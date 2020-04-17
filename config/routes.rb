bf2551ae798cd083013c9a2025c7cabccf032774
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-04-17 23:59:00 +0800

    appointment create page - save new appointment

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'doctor/index'
+  #get 'doctor/index'
   get 'welcome/index'
-  get 'patient/index'
-  get 'appointment/index'
+  #get 'patient/index'
+  #get 'appointment/index'
   
   resources :doctor
   resources :appointment
