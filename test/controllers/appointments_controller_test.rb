a3f2b4bc12a897ac38921ac6108ff8542648204f
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-04-21 23:58:22 +0800

    doctor controller tests

--- test/controllers/appointments_controller_test.rb
+++ test/controllers/appointments_controller_test.rb
@@ -3,7 +3,9 @@ require 'test_helper'
 class AppointmentsControllerTest < ActionDispatch::IntegrationTest
   test "should load appointments list page" do
     get appointments_path
+
     assert_response :success
+    assert_equal "index", @controller.action_name
   end
 
   test "should get schedule new appointment page" do
--- test/controllers/doctors_controller_test.rb
+++ test/controllers/doctors_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class DoctorsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get doctors_index_url
+  test "should load doctors list page" do
+    get doctors_path
+
     assert_response :success
+    assert_equal "index", @controller.action_name
   end
 
+  test "should new doctor page" do
+    get new_doctor_path
+    assert_response :success
+  end
 end
