98b025fa054e8eaba7c482e16a08585147c2ebaa
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-04-22 23:40:47 +0800

    adding tests for the appointments controller

--- test/controllers/appointments_controller_test.rb
+++ test/controllers/appointments_controller_test.rb
@@ -1,6 +1,19 @@
 require 'test_helper'
 
-class AppointmentsControllerTest < ActionDispatch::IntegrationTest
+class AppointmentsControllerTest < ActionDispatch::IntegrationTest  
+  
+  # called before every single test
+  setup do
+    @appointment = appointments(:aapointment1)
+    @testNewAppointment = appointments(:newAppointment)
+  end
+
+  # called after every single test
+  teardown do
+    # when controller is using cache it may be a good idea to reset it afterwards
+    Rails.cache.clear
+  end
+  
   test "should load appointments list page" do
     get appointments_path
 
