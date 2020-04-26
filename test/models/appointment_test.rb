dc2e69b240fdeb94136a967d65360abb5544b118
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-04-26 23:59:23 +0800

    more appoinment model tests

--- test/models/appointment_test.rb
+++ test/models/appointment_test.rb
@@ -1,6 +1,18 @@
 require 'test_helper'
 
-class AppointmentTest < ActiveSupport::TestCase
+class AppointmentTest < ActiveSupport::TestCase  
+  # called before every single test
+  setup do
+    @appointment = appointments(:appointment1)
+    @testNewAppointment = appointments(:newAppointment)
+  end
+
+  # called after every single test
+  teardown do
+    # when controller is using cache it may be a good idea to reset it afterwards
+    Rails.cache.clear
+  end
+  
   test "should not make appointments without appointment date" do
     appointment = Appointment.new
     assert_not appointment.save, "Created appointment without appointment date"
