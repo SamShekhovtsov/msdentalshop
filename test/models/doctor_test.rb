5a521bf3f673140978b11c15f3dc14012ad6e40e
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-04-27 02:17:19 +0800

    implementing tests for the doctor model

--- test/models/doctor_test.rb
+++ test/models/doctor_test.rb
@@ -14,9 +14,9 @@ class DoctorTest < ActiveSupport::TestCase
     Rails.cache.clear
   end
 
-  test "should not save doctor without name" do
+  test "should not save doctor without required parameteres" do
     doctor = Doctor.new
-    assert_not doctor.save, "Saved doctor without name"
+    assert_not doctor.save, "Saved doctor without required parameteres"
   end
 
   test "should save doctor with valid parameters" do
@@ -24,5 +24,29 @@ class DoctorTest < ActiveSupport::TestCase
     doctorValidAttributes.delete("id")
     doctor = Doctor.new(doctorValidAttributes)
     assert doctor.save, "Can't save doctor with valid attributes"
+  end
+
+  test "should NOT save doctor with missing attributes" do
+    doctorValidAttributes = @doctor.attributes
+    doctorValidAttributes.delete("id")
+    doctorValidAttributes.delete("level")
+    doctor = Doctor.new(doctorValidAttributes)
+    assert_not doctor.save, "Saved doctor with missing attributes"
+  end
+
+  test "should NOT save doctor with empty attributes" do
+    doctorValidAttributes = @doctor.attributes
+    doctorValidAttributes.delete("id")
+    doctorValidAttributes["level"] = ""
+    doctor = Doctor.new(doctorValidAttributes)
+    assert_not doctor.save, "Saved doctor with empty attributes"
+  end
+
+  test "should NOT save doctor with invalid attributes" do
+    doctorValidAttributes = @doctor.attributes
+    doctorValidAttributes.delete("id")
+    doctorValidAttributes["name"] = "test"
+    doctor = Doctor.new(doctorValidAttributes)
+    assert_not doctor.save, "Saved doctor with invalid attributes"
   end
 end
