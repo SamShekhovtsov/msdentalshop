9effceb93956abba525c18af2f6944399d6ba971
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-04-26 23:58:49 +0800

    appoinment model tests

--- test/models/appointment_test.rb
+++ test/models/appointment_test.rb
@@ -5,4 +5,10 @@ class AppointmentTest < ActiveSupport::TestCase
     appointment = Appointment.new
     assert_not appointment.save, "Created appointment without appointment date"
   end
+
+  test "should schedule an appointment with valid parameters" do
+
+    appointment = Appointment.new(@appointment.attributes.delete :id)
+    assert appointment.save, "Can't save an appointment"
+  end
 end
