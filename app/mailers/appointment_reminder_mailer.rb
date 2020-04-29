b1885c2672e3a97bc81f977e1942fb4518b4edc1
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-04-29 23:53:48 +0800

    implmenting appintment reminder mailer

--- app/mailers/appointment_reminder_mailer.rb
+++ app/mailers/appointment_reminder_mailer.rb
@@ -1,2 +1,8 @@
 class AppointmentReminderMailer < ApplicationMailer
+    default from: Rails.application.credentials.gmailsmtp[:username]
+
+    def send_appointment_reminder(contact)
+        @contact = contact
+        mail(to: Rails.application.credentials.gmailsmtp[:clinicinbox], from: @contact.email, :subject => "MS Dental clinic service appointment reminder")
+    end
 end
