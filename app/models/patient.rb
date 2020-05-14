56a905f94a337b7a54d1b668242213a4dae1e799
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-05-14 13:19:54 +0800

    adding patient email to views

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,5 @@
 class Patient < ApplicationRecord
     has_many :appointments, dependent: :destroy
     validates :name, presence: true, length: { minimum: 5 }
-    validates :email, presence: true, length: { minimum: 5 }
+    validates :email, presence: true, email: true, length: { minimum: 5 }, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
