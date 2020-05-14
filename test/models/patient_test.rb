069f96c16d97057f395769cfc8420c59d9166408
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-05-14 18:23:09 +0800

    patient model test improvements

--- test/models/patient_test.rb
+++ test/models/patient_test.rb
@@ -23,7 +23,9 @@ class PatientTest < ActiveSupport::TestCase
     patientValidAttributes = @patient.attributes
     patientValidAttributes.delete("id")
     patient = Patient.new(patientValidAttributes)
+    assert patient.valid?
     assert patient.save, "Can't save patient with valid attributes"
+    assert_equal 0, patient.errors.size
   end
 
   test "should NOT save patient with missing attributes" do
@@ -31,7 +33,9 @@ class PatientTest < ActiveSupport::TestCase
     patientValidAttributes.delete("id")
     patientValidAttributes.delete("name")
     patient = Patient.new(patientValidAttributes)
+    assert_not patient.valid?
     assert_not patient.save, "Saved patient with missing attributes"
+    assert_equal 2, patient.errors.size
   end
 
   test "should NOT save patient with empty attributes" do
@@ -39,7 +43,9 @@ class PatientTest < ActiveSupport::TestCase
     patientValidAttributes.delete("id")
     patientValidAttributes["name"] = ""
     patient = Patient.new(patientValidAttributes)
+    assert_not patient.valid?
     assert_not patient.save, "Saved patient with empty attributes"
+    assert_equal 2, patient.errors.size
   end
 
   test "should NOT save patient with invalid attributes" do
@@ -47,6 +53,8 @@ class PatientTest < ActiveSupport::TestCase
     patientValidAttributes.delete("id")
     patientValidAttributes["name"] = "test"
     patient = Patient.new(patientValidAttributes)
+    assert_not patient.valid?
     assert_not patient.save, "Saved patient with invalid attributes"
+    assert_equal 1, patient.errors.size
   end
 end
