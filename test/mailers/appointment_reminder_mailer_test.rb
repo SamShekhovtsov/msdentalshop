220bc8392732b0c5e997ebc210a5c14cd4ca7800
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-05-14 18:39:20 +0800

    action mailer tests added

--- test/mailers/appointment_reminder_mailer_test.rb
+++ test/mailers/appointment_reminder_mailer_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class AppointmentReminderMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @appointment = appointments(:appointment1)
+  end
+
+  # called after every single test
+  teardown do
+    # when controller is using cache it may be a good idea to reset it afterwards
+    Rails.cache.clear
+  end
+
+  test "should send the appointment reminder email" do
+    email = AppointmentReminderMailer.reminder_email(@appointment)
+    assert_emails 1 do
+      email.deliver_later
+    end
+
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    assert_equal email.to, [@appointment.patient.email]
+    assert_equal email.from, [Rails.application.credentials.gmailsmtp[:username]]
+    assert_equal email.subject, "MS Dental clinic service appointment reminder"
+    #assert_match 'appointment is arranged', email.body.encoded
+  end
 end
