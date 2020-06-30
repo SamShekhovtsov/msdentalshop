f68c819d870bee4ce8fec31d75259f07b1cebe5d
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-06-30 17:45:10 +0300

    adding controoler action restriction for authenticated users only

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,6 @@
 class AppointmentsController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create]
+
     def index
         @appointments = Appointment.all
     end
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,4 +1,6 @@
 class PatientsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  
   def index
     @patients = Patient.all
   end
