b3e38f9d44971ca2d21f72a893a3c0dba01233cf
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-07-02 20:09:34 +0300

    social login integration devise and Google OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  devise :rememberable, :omniauthable, omniauth_providers: [:google_oauth2]
+  attr_accessible :email
+
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+    unless user
+      user = User.create(email: data["email"] )
+    end
+    user
+  end
 end
