880d52e4a3588519fb3408af5b42e1c582f23534
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-07-03 13:14:20 +0300

    attempting to setup google authentication with devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,6 +272,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  # , access_type: "online"
+  config.omniauth :google_oauth2, Rails.application.credentials.dig(:auth0, :client_id), Rails.application.credentials.dig(:auth0, :client_secret), scope: 'userinfo.email,userinfo.profile'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
