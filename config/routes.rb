60667082b03f12f4b0670d8ec6f50a648f9fe12b
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-07-06 18:48:04 +0300

    integrating devise google oauth

--- config/routes.rb
+++ config/routes.rb
@@ -19,9 +19,15 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
   #devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
+
   devise_scope :user do
-    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    #root 'sessions#new'
+    #get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    #get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+    get '/users/sign_in', to: 'sessions#new', as: :new_user_session
+    post '/users/sign_in', to: 'devise/sessions#create', as: :user_session
+    get '/users/sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
 
   root 'welcome#index'
