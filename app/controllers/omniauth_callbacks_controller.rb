3cbfe1a50e2ed69e78bfe6860775640053c187fe
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-07-06 18:52:54 +0300

    devise google oauth integration

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -10,7 +10,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user.save!
 
     user.remember_me = true
-    sign_in(:user, user)
+    users_sign_in(:user, user)
 
     redirect_to after_sign_in_path_for(user)
   end
