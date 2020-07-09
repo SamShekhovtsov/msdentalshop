25b941b904b29636c11a0305b290c7f51dcd8c99
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-07-09 15:03:26 +0300

    trying to configure omniauth with devise

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,14 +1,20 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # replace with your authenticate method
-  skip_before_action :authenticate_user!
+  #include OmniauthHelper
+  #skip_before_action :authenticate_user!
 
   def google_oauth2
     auth = request.env["omniauth.auth"]
+    # Rails.logger.debug auth
+    # Rails.logger.debug auth["provider"]
+    # Rails.logger.debug auth["uid"]
+    # Rails.logger.debug auth["info"]["email"]
+    # Rails.logger.debug auth["info"]["name"]
     user = User.where(provider: auth["provider"], uid: auth["uid"])
             .first_or_initialize(email: auth["info"]["email"])
     user.name ||= auth["info"]["name"]
     user.save!
-
+    Rails.logger.debug user
     user.remember_me = true
     sign_in(:user, user)
 
